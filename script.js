34e466ce6b0da7bce9bb21b1c7b602d8f985d9ef
BennyG117 <bengomezp@gmail.com>
2023-05-23 09:55:59 -0700

    Initial Commit Benny Pack Light

---
+++ script.js
@@ -0,0 +1,67 @@
+
+function determineList(weatherAPI,Topic) {
+    //make a variable for the decision or make a variable for each possible packing list?
+    let decision = 
+
+    let
+    let
+    let
+    let
+    let
+    let
+    let
+    let
+
+hiking-cold
+    // if weather API returns temps <= 60 && topic is "hiking-cold" - select "hiking-cold" list 
+        //if weather API returns temps <= 60 && raining - select "hiking-cold" list 
+        //if weather API returns temps <= 60 && snowing - select "hiking-cold" list 
+
+fancy 
+    //if topic "fancy" is selected chose "fancy" list
+        //if weather API is raining && "fancy" is selected - make comment to add raing jacket
+        //if weather API is snowing && "fancy" is selected - make comment to add raing jacket & extra warm layers
+
+hiking-hot
+    //if weather API returns temps >= 71 && topic selected is "hiking-hot" then chose the "hiking-hot" list
+        //is weather API returns raining && "hiking-hot" is selected - make comment to add rain jacket
+
+beach-trip
+    //if weather API returns temps >= 60 && "beach-trip" is selected - select "beach-trip" list
+
+lake-trip
+    //if "lake-trip" topic is selected - select "lake-trip" list
+        //if weather API returns rain && "lake-trip" topic is selected - make comment to add rain jacket
+
+museum-trip        
+    //if weather API returns temps >= 60 && "museum-trip" tpocis is selected - select "museum-trip" list
+        //if weather API returns rain && "museum-trip" topic is selected - make comment to add rain jacket
+
+nerd-trip        
+    //if "nerd-trip" topic is selected - select "nerd-trip" list
+        //if weather API returns rain && "nerd-trip" topic is selected - make comment to add rain jacket & rain bag covers
+        //if weather API returns temps <= 59 && "nerd-trip" is selected - make comment to add a jacket
+
+snow-trip
+    //if weather API returns temps <= 60 && "snow-trip" is selected - select "snow-trip" list
+        //add comment to bring helment & goggles if needed
+
+hiking-rain        
+    //if weather API returns rain && topic is "hiking-rain" - select "hiking-rain" list
+
+
+}
+
+console.log(determineList);
+
+
+
+
+
+
+
+
+
+
+
+
